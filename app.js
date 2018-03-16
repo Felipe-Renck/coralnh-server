c12cabcf5668954305acf4a4e1092551d3ea34bd
rzabka <roger.zabka@gmail.com>
2018-03-16 16:56:33 -0300

    implementado o login

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var config = require('./config'); // get our config file
 var dbConnection = require('./dbconnection/dbconnection.js');
 app.set('secretString', config.secretString); // secret variable
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3002;
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -33,26 +33,25 @@ app.get('/', function (req, res, next) {
 app.post('/email', function (req, res, next) {
   var json = req.body;
   var Email = new email.Email(json.email, json.subject, json.message, 'Coral Jovem Novo Hamburgo');
-  Email.send().then(x => {res.send(common.ResolveStatusEmail(x))}).catch(x => res.send(common.ResolveStatusEmail(x)));
+  Email.send().then(x => { res.send(common.ResolveStatusEmail(x)) }).catch(x => res.send(common.ResolveStatusEmail(x)));
 });
 
 app.post('/user', function (req, res, next) {
   var newUser = req.body;
   console.log(newUser);
-  user.SaveUser(newUser).then(x => {res.send(common.ResolveStatusMongo(x))}).catch(x => res.send(common.ResolveStatusMongo(x)));
+  user.SaveUser(newUser).then(x => { res.send(common.ResolveStatusMongo(x)) }).catch(x => res.send(common.ResolveStatusMongo(x)));
 });
 
 app.get('/list-users', function (req, res, next) {
   if (req.query.usuario == "admin" && req.query.senha == "CoralNH@2018") {
-    user.GetAllUsers().then(x => {res.json(x), console.log(x)}).catch(x => res.json(x));
+    user.GetAllUsers().then(x => { res.json(x), console.log(x) }).catch(x => res.json(x));
   }
-  else{
+  else {
     res.send();
   }
 });
 
 app.post('/login', function (req, res) {
-
   var loggedUser = req.body;
   console.log(loggedUser.Email);
   // find the user
@@ -69,13 +68,13 @@ app.post('/login', function (req, res) {
     }
     else if (user) {
       // check if password matches
-      if (user.password != loggedUser.Password) {
+      if (user.toObject().password != loggedUser.Password) {
         res.status(401);
         res.json({ success: false, message: 'Senha incorreta' });
       } else {
 
         var payload = {
-          Email: user.email
+          Email: user.toObject().email
         }
         var token = jwt.sign(payload, app.get('secretString'), {
           expiresIn: 60 // expira em 60 segundos = 1 minuto (Tempo em segundos)
@@ -85,12 +84,22 @@ app.post('/login', function (req, res) {
         res.json({
           success: true,
           message: 'Autenticado',
+          isAdmin: user.toObject().isAdmin == undefined ? false : true,
           token: token
         });
       }
-
     }
+  });
+});
 
+app.get('/verifyToken', function (req, res) {
+  jwt.verify(req.query.token, config.secretString, function (err, decoded) {
+    if (err) {
+      res.send({ message: 'IsAExpiredToken' });
+    }
+    else {
+      res.send({ message: 'IsAValidToken' });
+    }
   });
 });
 
