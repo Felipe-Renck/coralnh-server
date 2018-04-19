a383af0ef00c88a32dbc4fe5f8add5aec2e81dee
rzabka <roger.zabka@gmail.com>
2018-04-18 23:11:54 -0300

    Removido o header

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use(bodyParser.json())
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "https://coral.jovensnh.com.br");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
